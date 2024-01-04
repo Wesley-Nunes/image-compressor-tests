0a815ee4ca328a269324649cc7ae356bdc93ba80
wesley-nunes <wesnmonteiro@gmail.com>
2024-01-04 16:16:47 -0300

    test: png and webP images

--- cypress/e2e/compress-image/basic_app_usage.cy.js
+++ cypress/e2e/compress-image/basic_app_usage.cy.js
@@ -29,14 +29,14 @@ describe("the core use of the app.", () => {
     );
   });
 
-  it("prevents compression of a file different from jpg, png, gif, or WebP.", () => {
+  it("prevents compression of a file different from jpg, png, or WebP.", () => {
     cy.get('[data-test-id="upload-image"]').selectFile(
       "cypress/fixture/human_feeding_the_little_squirrel (360p).mp4"
     );
 
     cy.get('[data-test-id="error-message"]').should(
       "have.text",
-      "Unsupported format: JPG, PNG, GIF, or WebP accepted"
+      "Unsupported format: JPG, PNG, or WebP accepted"
     );
   });
 });
